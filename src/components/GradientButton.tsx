e5e87a225aa1dade685cfe3aef5a9239c0556175
laupwing <loc_strict@hotmail.com>
2024-03-31 16:16:40 +0200

    Fix missing semicolon in GradientButton component

--- src/components/GradientButton.tsx
+++ src/components/GradientButton.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from "react"
 
 const GradientButton = () => {
-  return <div>GradientButton</div>;
-};
+    return <div>GradientButton</div>
+}
 
-export default GradientButton;
+export default GradientButton
