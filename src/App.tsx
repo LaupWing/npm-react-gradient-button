8a9d05362dc062a43afb27d8f83c9e13583f62e0
laupwing <loc_strict@hotmail.com>
2024-03-31 16:26:11 +0200

    Add GradientButton component and update background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import GradientButton from "./components/GradientButton"
+
 function App() {
     return (
-        <>
+        <div className="bg-red-400">
+            <GradientButton />
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </>
+        </div>
     )
 }
 
