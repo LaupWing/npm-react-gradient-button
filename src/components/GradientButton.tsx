1cf03910233acbf9710a4589fb6fcc3e9bb8483b
laupwing <loc_strict@hotmail.com>
2024-03-31 16:42:14 +0200

    Refactor GradientButton component and index.css

--- src/components/GradientButton.tsx
+++ src/components/GradientButton.tsx
@@ -1,14 +1,14 @@
 const GradientButton = () => {
     return (
-        <div
+        <button
             className={
-                "gradient-animation-border shadow hover:scale-[1.03] active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 transition duration-100 scale-100 "
+                "gradient-animation-border relative p-1 rounded-md overflow-hidden shadow hover:scale-[1.03] active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 transition duration-100 scale-100 "
             }
         >
-            <button className="rounded font-bold px-4 scale-100 bg-light dark:bg-dark text-gray-600 disabled:bg-gray-200 dark:text-gray-200 dark:disabled:bg-gray-700 bg-white py-[7px]">
-                test
-            </button>
-        </div>
+            <div className="font-bold rounded px-4 scale-100 dark:bg-dark text-gray-600 disabled:bg-gray-200 dark:text-gray-200 dark:disabled:bg-gray-700 bg-white py-[7px]">
+                Gradient Button
+            </div>
+        </button>
     )
 }
 
