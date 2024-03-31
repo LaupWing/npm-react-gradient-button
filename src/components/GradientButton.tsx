365ff420690e16a772ffb807bfca26eccad98493
laupwing <loc_strict@hotmail.com>
2024-03-31 16:24:00 +0200

    Update styles for GradientButton component

--- src/components/GradientButton.tsx
+++ src/components/GradientButton.tsx
@@ -5,7 +5,9 @@ const GradientButton = () => {
                 "gradient-animation-border shadow hover:scale-[1.03] active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 transition duration-100 scale-100 "
             }
         >
-            <button>test</button>
+            <button className="rounded font-bold px-4 scale-100 bg-light dark:bg-dark text-gray-600 disabled:bg-gray-200 dark:text-gray-200 dark:disabled:bg-gray-700 bg-white py-[7px]">
+                test
+            </button>
         </div>
     )
 }
