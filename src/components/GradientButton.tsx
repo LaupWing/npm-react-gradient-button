4b88224be528063e49cf9e043ffcb62d1726d180
laupwing <loc_strict@hotmail.com>
2024-03-31 16:20:59 +0200

    Refactor GradientButton component to include a styled button

--- src/components/GradientButton.tsx
+++ src/components/GradientButton.tsx
@@ -1,7 +1,13 @@
-import React from "react"
-
 const GradientButton = () => {
-    return <div>GradientButton</div>
+    return (
+        <div
+            className={
+                "gradient-animation-border shadow hover:scale-[1.03] active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 transition duration-100 scale-100 "
+            }
+        >
+            <button>test</button>
+        </div>
+    )
 }
 
 export default GradientButton
