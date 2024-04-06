c9606ca3cd66c72d392b11b26c5cc402ecf768e1
laupwing <loc_strict@hotmail.com>
2024-04-06 13:15:29 +0200

    Add custom colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
     content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                light: "rgb(var(--custom-light) / <alpha-value>)",
+                dark: "rgb(var(--custom-dark) / <alpha-value>)",
+                accent: "rgb(var(--custom-green) / <alpha-value>)",
+                "accent-light": "rgb(var(--custom-blue) / <alpha-value>)",
+                "custom-red": "rgb(var(--custom-red) / <alpha-value>)",
+                "custom-orange": "rgb(var(--custom-orange) / <alpha-value>)",
+                "custom-green": "rgb(var(--custom-green) / <alpha-value>)",
+                "custom-blue": "rgb(var(--custom-blue / <alpha-value>)",
+                "custom-purple": "rgb(var(--custom-purple) / <alpha-value>)",
+            },
+        },
     },
     plugins: [],
 }
